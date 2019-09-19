6b3bbbf330bde5c4ea1cd9206453b0ef5a9be619
Johan Ljungberg <johan.ljungberg@hotmail.com>
2019-09-19 16:37:22 +0200

    cards for news tab

--- app/controllers/NewsController.js
+++ app/controllers/NewsController.js
@@ -1,13 +1,33 @@
 (function(){
 
   angular.module('app')
-    .controller('NewsController', ['$state', NewsController]);
+    .controller('NewsController', ['$state', '$log', 'firebaseService', NewsController]);
 
-  function NewsController($state){
+  function NewsController($state, $log, firebaseService){
 
     var vm = this;
 
-    vm.message = 'News';
+    var onArticlesComplete = function(data){
+      $log.debug(data);
+      vm.articles = data;
+    }
+
+    var onError = function(reason){
+      $state.error = 'Error fetching the data';
+    }
+
+    firebaseService.getAllArticles().then(onArticlesComplete, onError);
+
+    /*const rootRef = firebase.database().ref().child('articles');
+    const ref = rootRef.child('article1');
+
+    var article = $firebaseObject(rootRef);
+
+    article.$loaded().then((function(data) {
+      $log.debug(data);
+      this.header = data.article1.header;
+      this.text = data.article1.text;
+    }).bind(this));*/
 
   }
 
---
+++ app/services/firebaseService.js
@@ -0,0 +1,35 @@
+(function(){
+
+  angular.module("app")
+    .factory('firebaseService', ['$firebaseObject', firebaseService]);
+
+  function firebaseService($firebaseObject) {
+
+    var config = {
+      apiKey: "AIzaSyDxgP8X48Ej-0xcX1U0g0X85LxXA6Q_fiA",
+      authDomain: "sportsteamwebpage-a77fc.firebaseapp.com",
+      databaseURL: "https://sportsteamwebpage-a77fc.firebaseio.com",
+      projectId: "sportsteamwebpage-a77fc",
+      storageBucket: "sportsteamwebpage-a77fc.appspot.com",
+      messagingSenderId: "310389626211",
+      appId: "1:310389626211:web:9aab1b3fc64bf3b5d46f56"
+    };
+
+    firebase.initializeApp(config);
+
+    var getAllArticles = function(){
+      return $firebaseObject(firebase.database().ref().child('articles'))
+                .$loaded().then(function(data) {
+                    return data;
+                });
+    };
+
+    return {
+        getAllArticles: getAllArticles
+    };
+
+  };
+
+
+
+}());
